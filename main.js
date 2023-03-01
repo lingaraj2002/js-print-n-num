e2b88991cb6d93ddfed86b2fec9e1189b370fe11
LINGARAJ <lingaraj01052002@gmail.com>
2023-03-01 12:16:01 +0530

    adding.

---
+++ main.js
@@ -0,0 +1,12 @@
+// Print N even numbers
+			
+var n=Number(prompt("Enter the number"));
+			
+			for(var k=1; k<=n; k++){
+				 if(k%2 == 0){
+					 document.write(k)
+				 }else{
+					  document.write(" ");
+					  
+					  }
+				 }
